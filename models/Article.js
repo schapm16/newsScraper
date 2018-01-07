74522cf27599f556ffa2b0f73e33f28ddef0ef74
Stephen Chapman <schapm16@gmail.com>
2018-01-07 20:57:26 +0000

    Revised Article model to ensure only unique articles are added to database.

--- models/Article.js
+++ models/Article.js
@@ -2,19 +2,22 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
-    title: String,
-    summary: String,
-    articleURL: String,
-    imageURL: String,
-    saved: {
-        type: Boolean,
-        default: false
-    },
-    comments: [{
-        type: Schema.Types.ObjectId,
-        ref: 'userComment'
-    }]
-    
+  title: String,
+  summary: String,
+  articleURL: {
+    type: String,
+    unique: true
+  },
+  imageURL: String,
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: 'userComment'
+  }]
+
 });
 
 module.exports = mongoose.model('Article', ArticleSchema);
