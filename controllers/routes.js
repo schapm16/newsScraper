4c944b82d564a8f544b41e6430ddf889e298a4a8
Stephen Chapman <schapm16@gmail.com>
2018-01-07 18:08:31 +0000

    Changed all route paths.  Wrote routes and database operations for userComments.

--- controllers/routes.js
+++ controllers/routes.js
@@ -15,6 +15,7 @@ function routes(app) {
       });
     }, function() {
       db.Article.find({}, function(err, data) {
+        if (err) throw err;
         console.log('Scrape Complete - Results Sent');
         res.json(data);
       });
@@ -23,21 +24,55 @@ function routes(app) {
 
   });
 
-  app.put('/save-article/:articleId', function(req, res) {
-    db.Article.findOneAndUpdate({ _id: req.params.articleId }, { saved: true }, {new: true}, function(err, data) {
+  app.put('/article/save/:articleId', function(req, res) {
+    db.Article.findOneAndUpdate({ _id: req.params.articleId }, { saved: true }, { new: true }, function(err, data) {
+      if (err) throw err;
       console.log('Article Saved');
       res.json(data);
     });
   });
 
-  app.put('/delete-article/:articleId', function(req, res) {
-    db.Article.findOneAndUpdate({ _id: req.params.articleId }, { saved: false }, {new: true}, function(err, data) {
+  app.put('/article/delete/:articleId', function(req, res) {
+    db.Article.findOneAndUpdate({ _id: req.params.articleId }, { saved: false }, { new: true }, function(err, data) {
+      if (err) throw err;
       console.log('Article Deleted');
       res.json(data);
     });
   });
+
+  app.post('/comment/add', function(req, res) {
+    db.userComment.create({ comment: req.body.comment }, function(err, data) {
+      if (err) throw err;
+      console.log('New Comment');
+      db.Article.findOneAndUpdate({ _id: req.body.articleId }, { $push: { comments: data._id } }, {new: true}, function(err, data) {
+        if (err) throw err;
+        console.log('Comment Saved To Article');
+        res.json(data);
+      });
+    });
+
+  });
   
-  // app.post('/add-note')
+  app.put('/comment/edit', function(req, res) {
+    db.userComment.findOneAndUpdate({_id: req.body.commentId}, {$set: {comment: req.body.comment}}, {new: true}, function(err, data) {
+      if (err) throw err;
+      console.log('Comment Edited');
+      res.json(data);
+    });
+  });
+  
+  app.delete('/comment/delete/:commentId', function(req, res) {
+    var commentId = req.params.commentId;
+    db.userComment.findOneAndRemove({_id: commentId}, function(err) {
+      if (err) throw err;
+      console.log('Comment Deleted');
+    });
+    db.Article.findOneAndUpdate({comments: commentId}, {$pull: {comments: commentId}}, {new: true}, function(err, data) {
+      if (err) throw err;
+      console.log('Reference To Comment Deleted From Article');
+      res.json(data);
+    });
+  });
 }
 
 module.exports = routes;
