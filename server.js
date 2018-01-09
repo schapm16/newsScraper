2e8bf948daffc941538542cc2ca5955ce786889f
Stephen Chapman <schapm16@gmail.com>
2018-01-09 07:35:10 +0000

    Deployment corrections

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Express
 var express = require('express');
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 
 // Body-Parser
 var bodyParser = require('body-parser');
@@ -12,13 +12,13 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 // Express-Handlebars
-var exphbs  = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Mongoose Connection To MongoDB
 var mongoose = require('mongoose');
-var MONGODB_URI = process.env.MONGODB_URI||'mongodb://localhost/mongoHeadlines';
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
 mongoose.connect(MONGODB_URI)
     .then(function() {
         console.log('Mongoose Connected');
